71e92aa50ae454a18b6cd210ad8999ba366ea6b0
Gilad Levi <gilad.x@gmail.com>
2023-03-07 13:59:10 +0200

    Commit from TeleportHQ

--- components/div.js
+++ components/div.js
@@ -7,20 +7,9 @@ const Div = (props) => {
   return (
     <>
       <div className="mySwiper swiper">
-        <div>
-          <DangerousHTML
-            html={`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />`}
-          ></DangerousHTML>
-        </div>
         <div className="swiper-wrapper">
           <div className="swiper-slide">
             <strong className="div-text">{props.slide1}</strong>
-          </div>
-          <div className="div-slide-2 swiper-slide">
-            <strong className="div-text1">{props.slide11}</strong>
-          </div>
-          <div className="div-slide-3 swiper-slide">
-            <strong className="div-text2">{props.slide111}</strong>
           </div>
         </div>
         <div>
@@ -59,34 +48,6 @@ const Div = (props) => {
             font-style: normal;
             font-weight: 700;
           }
-          .div-slide-2 {
-            flex: 0 0 auto;
-            width: 100%;
-            display: flex;
-            align-items: flex-start;
-            flex-direction: column;
-          }
-          .div-text1 {
-            width: 100%;
-            height: auto;
-            font-size: 5em;
-            font-style: normal;
-            font-weight: 700;
-          }
-          .div-slide-3 {
-            flex: 0 0 auto;
-            width: 100%;
-            display: flex;
-            align-items: flex-start;
-            flex-direction: column;
-          }
-          .div-text2 {
-            width: 100%;
-            height: auto;
-            font-size: 5em;
-            font-style: normal;
-            font-weight: 700;
-          }
           .div-root-class-name {
             top: 0px;
             left: 0px;
@@ -94,14 +55,6 @@ const Div = (props) => {
           }
           @media (max-width: 1200px) {
             .div-text {
-              color: var(--dl-color-scheme-green);
-              font-size: 4em;
-            }
-            .div-text1 {
-              color: var(--dl-color-scheme-green);
-              font-size: 4em;
-            }
-            .div-text2 {
               color: var(--dl-color-scheme-green);
               font-size: 4em;
             }
@@ -123,17 +76,13 @@ const Div = (props) => {
 }
 
 Div.defaultProps = {
-  slide11: 'WE BELIEVE IN ONE WAY: INTEGRITY',
   slide1: 'WE BELIEVE IN ONE WAY: INTEGRITY',
   rootClassName: '',
-  slide111: 'WE BELIEVE IN ONE WAY: INTEGRITY',
 }
 
 Div.propTypes = {
-  slide11: PropTypes.string,
   slide1: PropTypes.string,
   rootClassName: PropTypes.string,
-  slide111: PropTypes.string,
 }
 
 export default Div
