59d7811a8c518701c1be158bbce77504e7daba50
Gilad Levi <gilad.x@gmail.com>
2023-06-14 14:44:24 +0300

    Commit from TeleportHQ

---
+++ public/password.js
@@ -0,0 +1,13 @@
+        var password;
+
+        var pass1="700ta";
+
+        password=prompt('Please enter your password to view this page!','!Hit Backspace Before You start writing the
+        Password!');
+
+        if (password==pass1)
+        alert('Password Correct! Click OK to enter!');
+        else
+        {
+        window.location="/info";
+        }
